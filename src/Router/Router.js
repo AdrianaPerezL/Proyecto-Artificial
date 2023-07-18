55e055b20c3bf0e24cad73d6b72193b25ab13927
Nancy Ramírez <nancylissethramirezservellon@gmail.com>
2023-07-18 10:51:50 -0600

    nm

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from '../components/vistas/Index';
 import { Inicio } from '../components/vistas/Inicio';
 import Tabla  from '../components/vistas/Tabla'
+import { Expediente } from '../components/vistas/Expedientes/Expediente';
  
 export default function Router() {
   return (
@@ -10,6 +11,7 @@ export default function Router() {
       <Routes>
         <Route path='/' element={<Index/>}/>
         <Route path='/inicio' element={<Inicio/>}/>
+        <Route path='/expediente' element={<Expediente/>}/>
         <Route path='/tabla' element={<Tabla/>}/>
         </Routes>
     </BrowserRouter>
