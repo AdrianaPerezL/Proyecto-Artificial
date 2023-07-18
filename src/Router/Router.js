51a6242a91e5903bdc1dd37f316d43cee2fd5893
Alba <portilloalba92@gmail.com>
2023-07-18 10:15:56 -0600

    .
    .

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from '../components/vistas/Index';
 import { Inicio } from '../components/vistas/Inicio';
+import Tabla  from '../components/vistas/Tabla'
  
 export default function Router() {
   return (
@@ -9,6 +10,7 @@ export default function Router() {
       <Routes>
         <Route path='/' element={<Index/>}/>
         <Route path='/inicio' element={<Inicio/>}/>
+        <Route path='/tabla' element={<Tabla/>}/>
         </Routes>
     </BrowserRouter>
   );
