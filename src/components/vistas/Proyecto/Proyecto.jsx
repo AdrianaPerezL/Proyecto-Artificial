ec01cec4e6e8ae5668605b6d93ae9ae080de8f29
Alba <portilloalba92@gmail.com>
2023-07-19 13:48:26 -0600

    Modals editar

--- src/components/vistas/Proyecto/Proyecto.jsx
+++ src/components/vistas/Proyecto/Proyecto.jsx
@@ -8,7 +8,9 @@ function Tabla() {
     document.title = "Proyectos";
   }, []);
 
+  /* Modals */
   const [showModal, setShowModal] = React.useState(false);
+  const [showModal1, setShowModal1] = React.useState(false);
 
   return (
     <>
@@ -248,11 +250,151 @@ function Tabla() {
                             </span>
                           </button>
 
-                          <button className="btn btn-editar ">
+                          <button
+                            className="btn btn-editar "
+                            onClick={() => setShowModal1(true)}
+                          >
                             <span className="text-amarillo-editar text-2xl">
                               <FaRegEdit />
                             </span>
                           </button>
+                          {/* Empieza el modal */}
+                          {showModal1 ? (
+                            <>
+                              <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                                <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                                  {/*content*/}
+                                  <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                    {/*header*/}
+                                    <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                      <h3 className="title">
+                                        Editar Proyecto
+                                      </h3>
+                                      <button
+                                        className="p-1 ml-auto bg-transparent border-0 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                        onClick={() => setShowModal1(false)}
+                                      >
+                                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                          ×
+                                        </span>
+                                      </button>
+                                    </div>
+                                    {/*body*/}
+                                    <div className="relative p-6 flex-auto">
+                                      <div>
+                                        <div className="-mx-3 flex flex-wrap">
+                                          <div className="w-full px-3 sm:w-1/2">
+                                            <div className="mb-5">
+                                              <label
+                                                htmlFor="nproyecto"
+                                                className="mb-3 block text-base font-medium text-[#263562]"
+                                              >
+                                                Nombre proyecto
+                                              </label>
+                                              <input
+                                                type="text"
+                                                name="nproyecto"
+                                                id="nproyecto"
+                                                placeholder="Nombre proyecto"
+                                                className="w-full rounded-xl border border-[#E2231A] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#263562] focus:shadow-md"
+                                              />
+                                            </div>
+                                          </div>
+                                          <div className="w-full px-3 sm:w-1/2">
+                                            <div className="mb-5">
+                                              <label
+                                                htmlFor="pdefoto"
+                                                className="mb-3 block text-base font-medium text-[#263562]"
+                                              >
+                                                Expediente
+                                              </label>
+                                              <input
+                                                type="text"
+                                                name="Exp"
+                                                id="Exp"
+                                                placeholder="Expediente"
+                                                className="w-full rounded-xl border border-[#E2231A] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#263562] focus:shadow-md"
+                                              />
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div className="-mx-3 flex flex-wrap">
+                                          <div className="w-full px-3 sm:w-1/2">
+                                            <div className="mb-5">
+                                              <label
+                                                htmlFor="date"
+                                                className="mb-3 block text-base font-medium text-[#263562]"
+                                              >
+                                                Titulo de foto
+                                              </label>
+                                              <input
+                                                type="text"
+                                                name="tfoto"
+                                                id="tfoto"
+                                                placeholder="Titulo de foto"
+                                                className="w-full rounded-xl border border-[#E2231A] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#263562] focus:shadow-md"
+                                              />
+                                            </div>
+                                          </div>
+                                          <div className="w-full px-3 sm:w-1/2">
+                                            <div className="mb-5">
+                                              <label
+                                                htmlFor="pdefoto"
+                                                className="mb-3 block text-base font-medium text-[#263562]"
+                                              >
+                                                Pie de foto
+                                              </label>
+                                              <input
+                                                type="text"
+                                                name="pief"
+                                                id="pief"
+                                                placeholder="Pie de foto"
+                                                className="w-full rounded-xl border border-[#E2231A] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#263562] focus:shadow-md"
+                                              />
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div className="mb-5">
+                                          <label
+                                            htmlFor="guest"
+                                            className="mb-3 block text-base font-medium text-[#263562]"
+                                          >
+                                            Descripción
+                                          </label>
+                                          <input
+                                            type="text"
+                                            name="descripción"
+                                            id="descripción"
+                                            placeholder="Descripción"
+                                            className="w-full appearance-none rounded-xl border border-[#E2231A] bg-white py-8 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#263562] focus:shadow-md"
+                                          />
+                                        </div>
+                                      </div>
+                                    </div>
+                                    {/*footer*/}
+                                    <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                      <button
+                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={() => setShowModal1(false)}
+                                      >
+                                        Close
+                                      </button>
+                                      <button
+                                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={() => setShowModal1(false)}
+                                      >
+                                        Save Changes
+                                      </button>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                            </>
+                          ) : null}
+                          {/*Termina el modal*/}
                           <button className="btn btn-eliminar ">
                             <span className="text-rojo-eliminar text-xl">
                               <FaTrashAlt />
