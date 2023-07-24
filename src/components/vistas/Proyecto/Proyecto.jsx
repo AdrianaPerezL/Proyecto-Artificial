53905659b6bfabfcd1b85e91af3b8e6b20e830b8
AdrianaPerezL <112712237+AdrianaPerezL@users.noreply.github.com>
2023-07-23 19:14:17 -0600

    .

--- src/components/vistas/Proyecto/Proyecto.jsx
+++ src/components/vistas/Proyecto/Proyecto.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import { Paginacion } from "../../Componentes/Paginacion";
 import Swal from "sweetalert2";
 import axios from "axios";
+import '../../../assets/styles/proyecto.css'
 
 export const Tabla= () => {
   useEffect(() => {
@@ -111,7 +112,7 @@ export const Tabla= () => {
           <section className=" w-full relative overflow-x-auto shadow-md sm:rounded-lg pb-8">
             <div className="flex justify-between pt-24">
               <div></div>
-              <h1 className="text-center text-3xl text-black">PROYECTOS</h1>
+              <h1 className="text-center text-3xl text-black title-proyectos">PROYECTOS</h1>
               <div className="">
                 <div className="flex items-center md:justify-end px-5 m-2">
                   <a
@@ -123,7 +124,7 @@ export const Tabla= () => {
                       type="button"
                       onClick={() => setShowModal(true)}
                     >
-                      <span className="text-green-600 text-4xl">
+                      <span className="text-Negro text-4xl">
                         <FaPlusCircle />
                       </span>
                     </button>
@@ -216,14 +217,14 @@ export const Tabla= () => {
                       {/*footer*/}
                       <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                         <button
-                          className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                          className="mt-4 p-3 btn-cancel rounded-[10px] border"
                           type="button"
                           onClick={() => setShowModal(false)}
                         >
                           Cancelar
                         </button>
                         <button
-                          className="bg-emerald-500 text-black active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                          className="mt-4 p-3 btn-register rounded-[10px] border"
                           type="button"
                           onClick={() => setShowModal(false)}
                         >
@@ -394,7 +395,7 @@ export const Tabla= () => {
                                     {/*footer*/}
                                     <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                                       <button
-                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        className="mt-4 p-3 btn-register rounded-[10px] border"
                                         type="button"
                                         onClick={() => setShowModalver(false)}
                                       >
@@ -503,14 +504,14 @@ export const Tabla= () => {
                                     {/*footer*/}
                                     <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                                       <button
-                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        className="mt-4 p-3 btn-cancel rounded-[10px] border"
                                         type="button"
                                         onClick={() => setShowModal1(false)}
                                       >
                                         Cancelar
                                       </button>
                                       <button
-                                        className="bg-emerald-500 text-black active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        className="mt-4 p-3 btn-register rounded-[10px] border"
                                         type="button"
                                         onClick={() => setShowModal1(false)}
                                       >
